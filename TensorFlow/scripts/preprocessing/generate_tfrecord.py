8cceaaab7fcb9a60024953c232e9a741a8112cd5
Merah Soheyb <soheybemir@hotmail.fr>
2019-09-19 17:03:33 +0100

    setup

--- TensorFlow/scripts/preprocessing/generate_tfrecord.py
+++ TensorFlow/scripts/preprocessing/generate_tfrecord.py
@@ -42,6 +42,7 @@ def split(df, group):
 
 def create_tf_example(group, path):
     print("***************",group)
+    path = '/content/Custom-Object-Detector/TensorFlow/workspace/training_demo/images/train/'
     print("***************"+path)
     with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
         encoded_jpg = fid.read()
